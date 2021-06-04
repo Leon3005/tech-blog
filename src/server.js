f1fc7450455993366c2c95e5df992a10fa78fa43
Leon <leonwheeler08@gmail.com>
2021-06-04 07:41:39 +0100

    Added connection to DB with sequelize

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const connection = require("./config/connection");
+
 const PORT = process.env.PORT || 3005;
 
 const server = express();
@@ -9,6 +11,15 @@ server.use(cors());
 server.use(express.json({ extended: true }));
 server.use(express.urlencoded({ extended: true }));
 
-server.listen(PORT, () => {
-  console.log(`server is listening on http://localhost:${PORT}`);
-});
+const init = async () => {
+  try {
+    await connection.sync();
+    server.listen(PORT, () => {
+      console.log(`server is listening on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+init();
