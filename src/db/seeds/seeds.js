ae9dc21255968cba8e0065b520d8015d9eb62257
Leon <leonwheeler08@gmail.com>
2021-06-06 12:30:39 +0100

    Created seed files for users and blog posts

---
+++ src/db/seeds/seeds.js
@@ -0,0 +1,21 @@
+const sequelize = require("../../config/connection");
+const blogPost = require("../../models/blogPost");
+const user = require("../../models/user");
+const blogPosts = require("./blogPosts.json");
+const users = require("./users.json");
+
+const seed = async () => {
+  await sequelize.sync({ force: true });
+
+  await blogPost.bulkCreate(blogPosts);
+
+  console.log("Seeded blog posts");
+
+  await user.bulkCreate(users);
+
+  console.log("Seeded users");
+
+  process.exit(0);
+};
+
+seed();
