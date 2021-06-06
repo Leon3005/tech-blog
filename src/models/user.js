2c59a17e297a306b275d652dcb94b7a31bfdf4f6
Leon <leonwheeler08@gmail.com>
2021-06-06 13:33:13 +0100

    When a new user is created, the password is now encrypted

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
 
 const sequelize = require("../config/connection");
 
@@ -34,7 +35,31 @@ const schema = {
   },
 };
 
+const hooks = {
+  beforeCreate: async (newUser) => {
+    newUser.password = await bcrypt.hash(newUser.password, 10);
+    console.log(newUser.password);
+    return newUser;
+  },
+  beforeUpdate: async (updatedUser) => {
+    updatedUser.password = await bcrypt.hash(updatedUser.password, 10);
+    return updatedUser;
+  },
+  beforeBulkCreate: async (users) => {
+    const promises = users.map((user) => {
+      return bcrypt.hash(user.password, 10);
+    });
+
+    const passwords = await Promise.all(promises);
+
+    passwords.forEach((password, index) => {
+      users[index].password = password;
+    });
+  },
+};
+
 const options = {
+  hooks,
   sequelize,
   modelName: "user",
   timestamps: true,
