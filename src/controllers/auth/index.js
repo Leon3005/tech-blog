23d0501f4b1e700eff3fda081d7894b41b986562
Leon <leonwheeler08@gmail.com>
2021-06-08 05:57:36 +0100

    Added Bootstrap JS to layout and added logout function.

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -32,8 +32,19 @@ const login = async (req, res) => {
     console.error(err);
   }
 };
+
 const logout = (req, res) => {
-  res.send("logout");
+  try {
+    if (req.session.isLoggedIn) {
+      req.session.destroy(() => {
+        return res.status(200).json({ data: "Successfully logged out" });
+      });
+    } else {
+      return res.status(500).json({ error: "Failed to logout" });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: "Failed to logout" });
+  }
 };
 
 module.exports = { login, logout };
