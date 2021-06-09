eb10a1012488587842883a2b4cd852ffa1b525f7
Leon <leonwheeler08@gmail.com>
2021-06-09 21:02:56 +0100

    Adding different nav bards for logged in and logged out users

--- src/controllers/html/privateRender.js
+++ src/controllers/html/privateRender.js
@@ -1,6 +1,7 @@
 const renderDashboard = (req, res) => {
   try {
-    res.render("dashboard");
+    const { isLoggedIn } = req.session;
+    res.render("dashboard", { isLoggedIn });
   } catch (error) {
     console.log(error.message);
     res.status(500).json(error);
