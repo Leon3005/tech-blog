a47669673b96a766b1022bf123e394d4d0670cd5
Leon <leonwheeler08@gmail.com>
2021-06-14 07:30:31 +0100

    Added associations for comments, edited HTML

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,6 @@
 const User = require("./user");
 const BlogPost = require("./blogPost");
+const Comment = require("./comment");
 
 User.hasMany(BlogPost, {
   foreignKey: "user_id",
@@ -9,4 +10,20 @@ BlogPost.belongsTo(User, {
   foreignKey: "user_id",
 });
 
-module.exports = { User, BlogPost };
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, BlogPost, Comment };
